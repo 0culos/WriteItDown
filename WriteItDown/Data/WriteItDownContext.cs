e70e3cc627523f26906a6f12e7383ebe044b0556
0culos <danilopereira.contato@gmail.com>
2022-08-26 13:07:12 -0300

    Category CRUD scaffolding created

---
+++ WriteItDown/Data/WriteItDownContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WriteItDown.Models;
+
+namespace WriteItDown.Data
+{
+    public class WriteItDownContext : DbContext
+    {
+        public WriteItDownContext (DbContextOptions<WriteItDownContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WriteItDown.Models.Category> Category { get; set; }
+    }
+}
--- WriteItDown/Startup.cs
+++ WriteItDown/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WriteItDown.Data;
 
 namespace WriteItDown
 {
@@ -24,6 +26,9 @@ namespace WriteItDown
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<WriteItDownContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WriteItDownContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
